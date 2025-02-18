49bf7f5e22db4b0ebd2f5f58217c6d58511a8b05
BaoNgoc2801 <baongoc2801np@gmail.com>
2025-02-19 01:58:57 +0700

    modify UI

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -12,164 +12,93 @@ import {
 } from "react-icons/fa";
 
 const Contact = () => {
-  const { ref, inView } = useInView({
-    triggerOnce: true,
-    threshold: 0.2,
-  });
+  const { ref, inView } = useInView({ triggerOnce: true, threshold: 0.2 });
 
   return (
-    <div id="contact">
-      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 text-center py-12">
-        <motion.h2
-          ref={ref}
-          initial={{ opacity: 0, y: -100 }}
-          animate={inView ? { opacity: 1, y: 0 } : {}}
-          transition={{ delay: 0.3, duration: 0.5 }}
-          className="text-4xl font-bold text-white"
-        >
-          Contact <span className="text-purple-500">Me</span>
-        </motion.h2>
-        <motion.p
-          ref={ref}
-          initial={{ opacity: 0, y: -100 }}
-          animate={inView ? { opacity: 1, y: 0 } : {}}
-          transition={{ delay: 0.3, duration: 0.5 }}
-          className="text-slate-400 mt-4"
-        >
-          Let us collaborate and make good together
-        </motion.p>
-      </div>
-
-      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 mt-12 grig grid-cols-1 mg:grid-cols-2 gap-8">
-        <div className="space-y-6">
-          <motion.div
-            ref={ref}
-            initial={{ opacity: 0, x: -50 }}
-            animate={inView ? { opacity: 1, x: 0 } : {}}
-            transition={{ delay: 0.2, duration: 0.5 }}
-            className="flex items-center space-x-4"
+      <div id="contact">
+        <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 text-center py-12">
+          <motion.h2
+              ref={ref}
+              initial={{ opacity: 0, y: -50 }}
+              animate={inView ? { opacity: 1, y: 0 } : {}}
+              transition={{ delay: 0.1, duration: 0.3 }}
+              className="text-4xl font-bold text-white"
           >
-            <div className="bg-purple-500 p-4 rounded-full">
-              <FaPhone className="text-white w-6 h-6" />
-            </div>
-
-            <div>
-              <p className="text-lg font-medium text-purple-500">Call Me</p>
-              <p className="text-white">+334 040 072</p>
-            </div>
-          </motion.div>
-
-          <motion.div
-            ref={ref}
-            initial={{ opacity: 0, x: -50 }}
-            animate={inView ? { opacity: 1, x: 0 } : {}}
-            transition={{ delay: 0.4, duration: 0.5 }}
-            className="flex items-center space-x-4"
+            Contact <span className="text-purple-500">Me</span>
+          </motion.h2>
+          <motion.p
+              ref={ref}
+              initial={{ opacity: 0, y: -50 }}
+              animate={inView ? { opacity: 1, y: 0 } : {}}
+              transition={{ delay: 0.1, duration: 0.3 }}
+              className="text-slate-400 mt-4"
           >
-            <div className="bg-purple-500 p-4 rounded-full">
-              <FaEnvelope className="text-white w-6 h-6" />
-            </div>
-
-            <div>
-              <p className="text-lg font-medium text-purple-500">Email</p>
-              <p className="text-white">tlbngoc.work@gmail.com</p>
-            </div>
-          </motion.div>
-
-          <motion.div
-            ref={ref}
-            initial={{ opacity: 0, x: -50 }}
-            animate={inView ? { opacity: 1, x: 0 } : {}}
-            transition={{ delay: 0.6, duration: 0.5 }}
-            className="flex items-center space-x-4"
-          >
-            <div className="bg-purple-500 p-4 rounded-full">
-              <FaMapMarkedAlt className="text-white w-6 h-6" />
-            </div>
-
-            <div>
-              <p className="text-lg font-medium text-purple-500">Address</p>
-              <p className="text-white">Thu Duc, Ho Chi Minh</p>
-            </div>
-          </motion.div>
+            Let us collaborate and make good together
+          </motion.p>
         </div>
 
-        <motion.form
-          ref={ref}
-          initial={{ opacity: 0, x: 50 }}
-          animate={inView ? { opacity: 1, x: 0 } : {}}
-          transition={{ delay: 0.9, duration: 0.5 }}
-          className="space-y-4 text-white mt-6"
-        >
-          <div className="grid grid-cols-1 sm:grid-cols-2 gap-4">
-            <input
-              className="border border-purple-500 bg-gray-900 p-4 rounded-md w-full"
-              type="text"
-              placeholder="Full name"
+        <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 mt-12 grid grid-cols-1 md:grid-cols-2 gap-8">
+          <div className="space-y-6 ">
+            <ContactItem
+                icon={<FaPhone className="w-8 h-8" />}
+                title="Call Me"
+                content="+334 040 072"
+                link="tel:+334040072"
             />
-            <input
-              className="border border-purple-500 bg-gray-900 p-4 rounded-md w-full"
-              type="email"
-              placeholder="Your email"
+            <ContactItem
+                icon={<FaEnvelope className="w-8 h-8" />}
+                title="Email"
+                content="tlbngoc.work@gmail.com"
+                link="mailto:tlbngoc.work@gmail.com"
             />
-          </div>
-          <div className="grid grid-cols-1 sm:grid-cols-2 gap-4">
-            <input
-              className="border border-purple-500 bg-gray-900 p-4 rounded-md w-full"
-              type="text"
-              placeholder="Phone number"
-            />
-            <input
-              className="border border-purple-500 bg-gray-900 p-4 rounded-md w-full"
-              type="email"
-              placeholder="Budget"
+            <ContactItem
+                icon={<FaMapMarkedAlt className="w-8 h-8" />}
+                title="Address"
+                content="Thu Duc, Ho Chi Minh"
             />
           </div>
-          <textarea
-            className="border border-purple-500 bg-gray-900 p-4 rounded-md w-full"
-            placeholder="Message"
-          ></textarea>
-          <motion.button
-            whileHover={{ scale: 1.05 }}
-            whileTap={{ scale: 0.95 }}
-            className="bg-purple-500 text-white px-6 py-3 rounded-md hover:bg-purple-600 transition duration-200"
-            type="submit"
-          >
-            Submit Message
-          </motion.button>
-        </motion.form>
-      </div>
-      {/*Footer */}
-      <motion.div
-        ref={ref}
-        initial={{ opacity: 0 }}
-        animate={inView ? { opacity: 1 } : {}}
-        transition={{ delay: 1, duration: 0.5 }}
-        className="mt-48 flex justify-between items-center p-5 text-white  border-t-2 border-purple-500"
-      >
-        <p>2022. All Rights Reserved</p>
-        <p>Frontend Developer</p>
-        <div className="flex justify-center space-x-4 text-white mt-4">
-          <a className="hover:text-purple-500">
-            {" "}
-            <FaFacebook className="w-6 h-6" />{" "}
-          </a>
-          <a className="hover:text-purple-500">
-            {" "}
-            <FaTwitter className="w-6 h-6" />{" "}
-          </a>
-          <a className="hover:text-purple-500">
-            {" "}
-            <FaLinkedin className="w-6 h-6" />{" "}
-          </a>
-          <a className="hover:text-purple-500">
-            {" "}
-            <FaInstagram className="w-6 h-6" />{" "}
-          </a>
+
         </div>
-      </motion.div>
-    </div>
+          <div>
+              <motion.div
+                  ref={ref}
+                  initial={{opacity: 0}}
+                  animate={inView ? {opacity: 1} : {}}
+                  transition={{delay: 0.4, duration: 0.3}}
+                  className="mt-48 flex flex-col items-center text-center p-5 text-white border-t-2 border-purple-500"
+              >
+                  <p>2025. All Rights Reserved</p>
+                  <p>About this website: built with ReactJS, JavaScript, TailwindCSS, Framer Motion, and Vercel
+                      hosting.</p>
+              </motion.div>
+          </div>
+
+      </div>
   );
 };
+
+const ContactItem = ({icon, title, content, link}) => (
+    <motion.div
+        initial={{opacity: 0, x: -30}}
+        animate={{opacity: 1, x: 0}}
+        transition={{duration: 0.3}}
+        className="flex items-center space-x-4"
+    >
+        <div className="bg-purple-500 p-4 rounded-full text-white w-12 h-12 flex items-center justify-center">
+            {icon}
+        </div>
+        <div>
+            <p className="text-lg font-medium text-purple-500">{title}</p>
+        {link ? (
+            <a href={link} className="text-white hover:text-purple-400 transition">
+              {content}
+            </a>
+        ) : (
+            <p className="text-white">{content}</p>
+        )}
+      </div>
+    </motion.div>
+);
+
 
 export default Contact;
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,7 +5,7 @@ import { Link } from "react-scroll";
 
 const Hero = () => {
   return (
-    <div className="text-white py-10 mt-20">
+    <div className="text-white py-10 mt-20 ">
       <motion.img
         initial={{ opacity: 0, sacle: 0 }}
         animate={{ opacity: 1, sacle: 1 }}
@@ -22,7 +22,7 @@ const Hero = () => {
         transition={{
           duration: 0.5,
         }}
-        className="container mx-auto text-center"
+        className="container mx-auto text-center mt-20"
       >
         <motion.h1
           initial={{ opacity: 0, y: 100 }}
@@ -46,29 +46,28 @@ const Hero = () => {
           transition={{ delay: 1.2, duration: 0.5 }}
           className="text-gray-400 text-2xl mb-8"
         >
-          Welcome to My Personal Website
-        </motion.p>
+            I enjoy building sites & apps. My focus is ReactJS        </motion.p>
         <div className="flex justify-center space-x-4 mb-10">
-          <motion.button
-            initial={{ opacity: 0, x: -100 }}
-            animate={{ opacity: 1, x: 0 }}
-            transition={{ delay: 1.5, duration: 0.5 }}
-            className="bg-purple-500 text-white px-6 py-3 rounded-full"
-          >
-            <Link to="contact" smooth={true} duration={500} offset={-70}>
-              Hire Me
-            </Link>
-          </motion.button>
-          <motion.button
-            initial={{ opacity: 0, x: 100 }}
-            animate={{ opacity: 1, x: 0 }}
-            transition={{ delay: 1.5, duration: 0.5 }}
-            className="text-white border-white px-6 py-3 rounded-full"
-          >
-            <Link to="about" smooth={true} duration={500} offset={-70}>
-              My Story
-            </Link>
-          </motion.button>
+          {/*<motion.button*/}
+          {/*  initial={{ opacity: 0, x: -100 }}*/}
+          {/*  animate={{ opacity: 1, x: 0 }}*/}
+          {/*  transition={{ delay: 1.5, duration: 0.5 }}*/}
+          {/*  className="bg-purple-500 text-white px-6 py-3 rounded-full"*/}
+          {/*>*/}
+          {/*  <Link to="contact" smooth={true} duration={500} offset={-70}>*/}
+          {/*    Hire Me*/}
+          {/*  </Link>*/}
+          {/*</motion.button>*/}
+          {/*<motion.button*/}
+          {/*  initial={{ opacity: 0, x: 100 }}*/}
+          {/*  animate={{ opacity: 1, x: 0 }}*/}
+          {/*  transition={{ delay: 1.5, duration: 0.5 }}*/}
+          {/*  className="text-white border-white px-6 py-3 rounded-full"*/}
+          {/*>*/}
+          {/*  <Link to="about" smooth={true} duration={500} offset={-70}>*/}
+          {/*    My Story*/}
+          {/*  </Link>*/}
+          {/*</motion.button>*/}
         </div>
       </motion.div>
     </div>
