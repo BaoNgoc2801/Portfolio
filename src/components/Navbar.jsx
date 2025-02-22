cc756471a2b36e734ea936ac4c930b5f5e382969
BaoNgoc2801 <baongoc2801np@gmail.com>
2025-02-22 17:02:14 +0700

    modify UI

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -116,8 +116,8 @@ const Navbar = () => {
                     animate={{opacity: 1, x: 0, y: 0}}
                     transition={{duration: 0.5}}
                     className="text-xl font-bold flex items-center gap-2 py-6 px-4">
-                    <span className="text-white">IRON</span>
-                    <span className="text-purple-500">Coding</span>
+                    <span className="text-white">FRONTEND</span>
+                    <span className="text-purple-500">Developer</span>
                 </motion.div>
             </div>
         </div>
